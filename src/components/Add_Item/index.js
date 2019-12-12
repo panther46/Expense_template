f5d42ca4d875a82155efc00ad65e9a3749f73bd7
Samir M R <samir@Samirs-MacBook-Pro.local>
2019-12-12 15:30:44 +0100

    Version 1.2 Building adding item form

--- src/components/Add_Item/index.js
+++ src/components/Add_Item/index.js
@@ -5,8 +5,49 @@ import React from 'react';
 
 function AddItem(){
     return(
-        <h1>From add Item</h1>
+        <div className = "container">
+            <form action="#">
+                <div className="row">
+                    <div className="col s12">
+                        <p>Item Name</p>
+                            <div className="input-field col s12">
+                            <input type="text" id="autocomplete-input" class="autocomplete"/>
+                            <label for="autocomplete-input">Autocomplete</label>
+                            </div>
+                            <p>Item Price</p>
+                            <div className="input-field col s12">
+                            <input type="text" id="autocomplete-input" class="autocomplete"/>
+                            <label for="autocomplete-input">Autocomplete</label>
+                            </div>
+                    </div>
+                    <div className = "col s12">
+                    <p>
+                        <label>
+                        <input class="with-gap" name="group1" type="radio"  />
+                        <span>Desert</span>
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                        <input class="with-gap" name="group1" type="radio"  />
+                        <span>Salad</span>
+                        </label>
+                    </p>
+                    <p>
+                        <label>
+                        <input class="with-gap" name="group1" type="radio"  />
+                        <span>Drink</span>
+                </label>
+                </p>
+                    </div>
+            
+            </div>            
+            </form> 
+        </div>   
     )
 }
+
+
+
 
 export default AddItem;
